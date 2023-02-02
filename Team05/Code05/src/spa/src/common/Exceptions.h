4cf8008babaf265a1ee04425c26ef466b3cba9d0
okg00dbye <E0425498@u.nus.edu>
2023-02-03 00:24:24 +0800

    Create PQL Syntax and Semantic Exceptions classes

--- Team05/Code05/src/spa/src/common/Exceptions.h
+++ Team05/Code05/src/spa/src/common/Exceptions.h
@@ -1,1 +1,21 @@
-class Exceptions {};
+#pragma once
+
+#include <exception>
+
+class InvalidPqlException : public std::exception {
+ public:
+  InvalidPqlException(char *msg) : message(msg) {};
+  char *what() { return message; }
+ private:
+  char *message;
+};
+
+class PqlSyntaxErrorException : public InvalidPqlException { ;
+ public:
+  PqlSyntaxErrorException(char *msg) : InvalidPqlException(msg) {}
+};
+
+class PqlSemanticErrorException : public InvalidPqlException {
+ public:
+  PqlSemanticErrorException(char *msg) : InvalidPqlException(msg) {};
+};

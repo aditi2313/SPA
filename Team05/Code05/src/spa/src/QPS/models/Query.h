7562e9a0f20e3baef1002a69d81bfccb2112306f
okg00dbye <E0425498@u.nus.edu>
2023-02-03 00:24:55 +0800

    Add synonym declarations to Query class

--- Team05/Code05/src/spa/src/QPS/models/Query.h
+++ Team05/Code05/src/spa/src/QPS/models/Query.h
@@ -2,7 +2,10 @@
 
 #include <vector>
 #include <memory>
+#include <unordered_map>
+
 #include "Clause.h"
+#include "models/EntityStub.h"
 
 namespace qps {
 class Query {
@@ -10,8 +13,26 @@ class Query {
   inline std::vector<std::unique_ptr<Clause>> &get_clauses() {
     return clauses_;
   }
+
+  inline void add_clause(std::unique_ptr<Clause> &&clause) {
+    clauses_.push_back(std::move(clause));
+  }
+
+  // Returns false if synonym already exists in the declaration hashmap.
+  inline bool set_synonym(std::string synonym, models::EntityStub entity) {
+    if (synonym_declarations_.find(synonym) != synonym_declarations_.end())
+      return false;
+    synonym_declarations_[synonym] = entity;
+    return true;
+  }
+
+  inline models::EntityStub get_synonym(std::string synonym) {
+    return synonym_declarations_.at(synonym);
+  }
+
  private:
   std::vector<std::unique_ptr<Clause>> clauses_;
+  std::unordered_map<std::string, models::EntityStub> synonym_declarations_;
 };
 }  // namespace qps
 

53e01313ea5eb82dc8cbc116c320d4cf588ece24
okg00dbye <E0425498@u.nus.edu>
2023-02-03 00:43:59 +0800

    Write ParseQuery function

--- Team05/Code05/src/spa/src/QPS/preprocessor/Parser.cpp
+++ Team05/Code05/src/spa/src/QPS/preprocessor/Parser.cpp
@@ -1,3 +1,45 @@
+#include <sstream>
+
 #include "Parser.h"
 
-namespace qps {}  // namespace qps
+namespace qps {
+
+bool Parser::ShouldGoToNextState(int current_state_index, std::string token) {
+  if (current_state_index >= states_.size() - 1) return false; // last stage
+  return token == states_.at(current_state_index + 1)->kTransitionKeyword;
+}
+
+// Inserts whitespaces in front of semicolons for
+// easier delimitation in ParseQuery
+std::string PreprocessQueryString(std::string query_string) {
+  std::string output = "";
+  for (char c : query_string) {
+    if (c == ';') output += ' ';
+    output += c;
+  }
+  return output;
+}
+
+Query Parser::ParseQuery(std::string query_string) {
+  Query query;
+  std::stringstream ss(PreprocessQueryString(query_string));
+  std::vector<std::string> tokens;
+  std::string token;
+  while (ss >> token) {
+    // Separates query string by whitespace.
+    // Works for multiple whitespaces too.
+    tokens.push_back(token);
+  }
+
+  int current_state_index = 0;
+  auto itr = tokens.begin();
+  while (itr != tokens.end()) {
+    if (ShouldGoToNextState(current_state_index, *itr)) {
+      continue; // go to next state
+    }
+    itr = states_.at(current_state_index)->parse(tokens, itr, query);
+  }
+
+  return query;
+}
+}  // namespace qps
--- Team05/Code05/src/spa/src/QPS/preprocessor/Parser.h
+++ Team05/Code05/src/spa/src/QPS/preprocessor/Parser.h
@@ -1,3 +1,59 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <unordered_map>
+
+#include "../models/Query.h"
+#include "ParseState.h"
+#include "models/EntityStub.h"
+
 namespace qps {
-class Parser {};
+class Parser {
+ public:
+  Parser() {
+    states_.emplace_back(std::make_unique<DeclarationParseState>());
+    states_.emplace_back(std::make_unique<SynonymParseState>());
+    states_.emplace_back(std::make_unique<SuchThatParseState>());
+    states_.emplace_back(std::make_unique<PatternParseState>());
+  }
+
+  Query ParseQuery(std::string query_string);
+
+  static bool is_design_entity(std::string identifier) {
+    return design_entity_identifiers_.find(identifier) != design_entity_identifiers_.end();
+  }
+
+  static models::EntityStub get_design_entity(std::string identifier) {
+    return design_entity_identifiers_.at(identifier);
+  }
+
+  static inline bool is_ident(std::string str) {
+    if (str.empty() || !isalpha(str[0])) return false;
+    for (char c : str) {
+      if (!isalnum(c)) return false;
+    }
+    return true;
+  }
+
+ private:
+  std::vector<std::unique_ptr<ParseState>> states_{};
+  std::vector<int> arr{1};
+  bool ShouldGoToNextState(int current_state_index, std::string token);
+  std::string PreprocessQueryString(std::string query_string);
+
+  static inline std::unordered_map<std::string, models::EntityStub> design_entity_identifiers_{
+      {"stmt", models::EntityStub()},
+      {"read", models::EntityStub()},
+      {"print", models::EntityStub()},
+      {"call", models::EntityStub()},
+      {"while", models::EntityStub()},
+      {"if", models::EntityStub()},
+      {"assign", models::EntityStub()},
+      {"variable", models::EntityStub()},
+      {"constant", models::EntityStub()},
+      {"procedure", models::EntityStub()},
+  };
+
+};
 }  // namespace qps

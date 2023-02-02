6cbd69bd64bb6cea84c215457b025b3d9759c6e4
aditi2313 <guptaaditi2313@gmail.com>
2023-02-02 21:16:16 +0800

    PR changes.

--- Team05/Code05/src/spa/src/PKB/Table.cpp
+++ Team05/Code05/src/spa/src/PKB/Table.cpp
@@ -1,10 +1,3 @@
 #include "Table.h"
 
-namespace pkb {
-
-    template<class T>
-    T Table<T>::get_row(int num) {
-        return rows_.at(num);
-    }
-
-}  // namespace pkb
+namespace pkb {}  // namespace pkb

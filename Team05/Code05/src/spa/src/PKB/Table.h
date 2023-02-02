42a330bfc03a7310c02beb45a85ae7686a4b904b
aditi2313 <guptaaditi2313@gmail.com>
2023-02-02 15:25:41 +0800

    cpplint issues

--- Team05/Code05/src/spa/src/PKB/Table.h
+++ Team05/Code05/src/spa/src/PKB/Table.h
@@ -8,11 +8,11 @@ namespace pkb {
 
 template <class T>
 class Table {
-public:
- std::vector<T> rows;
- void addRow(T row);
- T getRow(int num);
+ public:
+  std::vector<T> rows;
+  void addRow(T row);
+  T getRow(int num);
 };
 
 #endif  // SPA_TABLE_H
-}
+}  // namespace pkb

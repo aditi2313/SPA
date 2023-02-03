40cc6ddd08861eb720f4a0f7f955781dfcf01b24
okg00dbye <E0425498@u.nus.edu>
2023-02-03 16:30:46 +0800

    Write a unit test for is_ident

---
+++ Team05/Code05/src/unit_testing/src/QPS/preprocessor/ParserTest.cpp
@@ -0,0 +1,17 @@
+#include <catch.hpp>
+
+#include "QPS/preprocessor/Parser.h"
+
+using namespace qps; // NOLINT
+
+TEST_CASE("Test Parser") {
+  Parser parser;
+  SECTION("Validation methods") {
+    REQUIRE(Parser::is_ident("a") == true);
+    REQUIRE(Parser::is_ident("abc") == true);
+    REQUIRE(Parser::is_ident("abc0123") == true);
+    REQUIRE(Parser::is_ident("1") == false);
+    REQUIRE(Parser::is_ident("a&") == false);
+  };
+
+}

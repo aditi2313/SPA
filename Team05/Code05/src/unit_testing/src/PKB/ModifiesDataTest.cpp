95d9573d910d172fe1013bd555a1c2c0aedbfa98
aditi2313 <guptaaditi2313@gmail.com>
2023-02-03 13:48:18 +0800

    ModifiesTable and ModifiesData unit tests.

--- Team05/Code05/src/unit_testing/src/PKB/ModifiesDataTest.cpp
+++ Team05/Code05/src/unit_testing/src/PKB/ModifiesDataTest.cpp
@@ -7,9 +7,11 @@ using namespace pkb; // NOLINT
 
 TEST_CASE("Test ModifiesData") {
     ModifiesData modifies_data;
-    std::vector<std::string> variables{"a", "b", "c"};
+    std::vector<std::string> variables{"a"};
     modifies_data.line_ = 10;
-    modifies_data.variables_ = variables;
+    modifies_data.variables_ = std::vector<std::string> {"a"};
 
-    REQUIRE(((modifies_data.line_ == 10) && (modifies_data.variables_.at(0) == "a")));
+    SECTION("Retrieving ModifiesData attributes") {
+        REQUIRE(((modifies_data.line_ == 10) && (modifies_data.variables_.at(0) == "a")));
+    };
 }
--- Team05/Code05/src/unit_testing/src/PKB/ModifiesTable.cpp
+++ Team05/Code05/src/unit_testing/src/PKB/ModifiesTable.cpp
@@ -9,11 +9,13 @@ using namespace pkb; // NOLINT
 TEST_CASE("Test ModifiesTable") {
     ModifiesTable modifies_table;
     ModifiesData modifies_data;
-    std::vector<std::string> variables{"a", "b", "c"};
+    std::vector<std::string> variables{"a"};
     modifies_data.line_ = 10;
     modifies_data.variables_ = variables;
-
     modifies_table.add_row(modifies_data);
-    ModifiesData retrieved_modifies_data = modifies_table.get_row(0);
-    REQUIRE(((retrieved_modifies_data.line_ == 10) && (retrieved_modifies_data.variables_.at(0) == "a")));
+
+    SECTION("Retrieving ModifiesTable row") {
+        ModifiesData retrieved_modifies_data = modifies_table.get_row(0);
+        REQUIRE(((retrieved_modifies_data.line_ == 10) && (retrieved_modifies_data.variables_.at(0) == "a")));
+    };
 }

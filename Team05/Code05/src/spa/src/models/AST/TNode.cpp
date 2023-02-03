49cb6433f0e3e64d13700ae423dcd31915d662d8
nhjryan <e0544583@u.nus.edu>
2023-02-03 23:46:18 +0800

    Fix even more codestyle

--- Team05/Code05/src/spa/src/models/AST/TNode.cpp
+++ Team05/Code05/src/spa/src/models/AST/TNode.cpp
@@ -8,6 +8,6 @@ void ProgramNode::add_proc(std::unique_ptr<ProcNode> proc) {
 
 void StmtLstNode::add_stmt(std::unique_ptr<StatementNode> stmt) {
   stmts_.push_back(std::move(stmt));
-};
+}
 
 }  // namespace ast

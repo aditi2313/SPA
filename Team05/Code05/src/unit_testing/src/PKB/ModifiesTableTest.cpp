eb2bb6beb0d7285b26fd1c4e955354ce1087a444
Au Chen Xi, Gabriel <au.gabriel@yahoo.com.sg>
2023-02-03 20:03:24 +0800

    Fix tests

--- Team05/Code05/src/unit_testing/src/PKB/ModifiesTableTest.cpp
+++ Team05/Code05/src/unit_testing/src/PKB/ModifiesTableTest.cpp
@@ -1,21 +1,21 @@
+#include <catch.hpp>
 #include <string>
 #include <vector>
-#include <catch.hpp>
 
-#include "PKB/ModifiesTable.h"
-#include "PKB/ModifiesData.h"
+#include "PKB/data/ModifiesData.h"
+#include "PKB/tables/IndexableTable.h"
 
-using namespace pkb; // NOLINT
+using namespace pkb;  // NOLINT
 
 TEST_CASE("Test ModifiesTable") {
-    ModifiesTable modifies_table;
-    std::vector<std::string> variables{"a"};
-    ModifiesData modifies_data(10, variables);
-    modifies_table.add_row(modifies_data);
+  ModifiesTable modifies_table;
+  std::vector<std::string> variables{"a"};
+  ModifiesData modifies_data(10, variables);
+  modifies_table.add_row(5, modifies_data);
 
-    SECTION("Retrieving ModifiesTable row") {
-        ModifiesData retrieved_modifies_data = modifies_table.get_row(0);
-        REQUIRE(((retrieved_modifies_data.line_ == 10)
-                 && (retrieved_modifies_data.variables_.at(0) == "a")));
-    };
+  SECTION("Retrieving ModifiesTable row") {
+    ModifiesData retrieved_modifies_data = modifies_table.get_row(5);
+    REQUIRE(((retrieved_modifies_data.get_line() == 10) &&
+             (retrieved_modifies_data.get_variables().at(0) == "a")));
+  };
 }

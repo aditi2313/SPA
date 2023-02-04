7a78edcebdd86b248ddfc40c917c882caca4875b
Au Chen Xi, Gabriel <au.gabriel@yahoo.com.sg>
2023-02-04 21:41:48 +0800

    Fix checkstyle

--- Team05/Code05/src/spa/src/QPS/models/Clause.h
+++ Team05/Code05/src/spa/src/QPS/models/Clause.h
@@ -8,41 +8,41 @@
 #include "PKB/PKBRead.h"
 #include "QueryResult.h"
 
-    namespace qps {
-  // Pure abstract base class for a Clause:
-  // It should not be instantiated as its own object.
-  class Clause {
-   public:
-    Clause(Argument arg1, Argument arg2) : arg1(arg1), arg2(arg2) {}
+namespace qps {
+// Pure abstract base class for a Clause:
+// It should not be instantiated as its own object.
+class Clause {
+ public:
+  Clause(Argument arg1, Argument arg2) : arg1(arg1), arg2(arg2) {}
 
-    virtual QueryResult Evaluate(std::unique_ptr<pkb::PKBRead> pkb) = 0;
-    virtual ~Clause() = 0;
+  virtual QueryResult Evaluate(std::unique_ptr<pkb::PKBRead> pkb) = 0;
+  virtual ~Clause() = 0;
 
-    bool operator==(Clause const &other) const {
-      const std::type_info &ti1 = typeid(*this);
-      const std::type_info &ti2 = typeid(other);
+  bool operator==(Clause const &other) const {
+    const std::type_info &ti1 = typeid(*this);
+    const std::type_info &ti2 = typeid(other);
 
-      return ti1 == ti2 && arg1 == other.arg1 && arg2 == other.arg2;
-    }
+    return ti1 == ti2 && arg1 == other.arg1 && arg2 == other.arg2;
+  }
 
-    inline bool operator!=(Clause const &other) const {
-      return !(*this == other);
-    }
+  inline bool operator!=(Clause const &other) const {
+    return !(*this == other);
+  }
 
-   protected:
-    Argument arg1;
-    Argument arg2;
-  };
+ protected:
+  Argument arg1;
+  Argument arg2;
+};
 
-  class ModifiesClause : public Clause {
-   public:
-    QueryResult Evaluate(std::unique_ptr<pkb::PKBRead> pkb) override;
-    ModifiesClause(Argument arg1, Argument arg2) : Clause(arg1, arg2) {}
-  };
+class ModifiesClause : public Clause {
+ public:
+  QueryResult Evaluate(std::unique_ptr<pkb::PKBRead> pkb) override;
+  ModifiesClause(Argument arg1, Argument arg2) : Clause(arg1, arg2) {}
+};
 
-  class PatternClause : public Clause {
-   public:
-    QueryResult Evaluate(std::unique_ptr<pkb::PKBRead> pkb) override;
-    PatternClause(Argument arg1, Argument arg2) : Clause(arg1, arg2) {}
-  };
+class PatternClause : public Clause {
+ public:
+  QueryResult Evaluate(std::unique_ptr<pkb::PKBRead> pkb) override;
+  PatternClause(Argument arg1, Argument arg2) : Clause(arg1, arg2) {}
+};
 }  // namespace qps

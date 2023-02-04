984c4b745ec4257b51d69f547563f061b79c05fc
nhjryan <e0544583@u.nus.edu>
2023-02-04 17:54:22 +0800

    Fix codestyle issues

--- Team05/Code05/src/spa/src/models/AST/TNode.h
+++ Team05/Code05/src/spa/src/models/AST/TNode.h
@@ -3,7 +3,7 @@
 namespace ast {
 class TNode {
  public:
-  // TODO: implement accept_visitor for design extractor
+  // TODO(nhjryan): implement accept_visitor for design extractor
   virtual void accept_visitor() {}
 };
 }  // namespace ast

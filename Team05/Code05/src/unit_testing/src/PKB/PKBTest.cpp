a19852238420449529db1a8849d9b8d5ce13c927
Au Chen Xi, Gabriel <au.gabriel@yahoo.com.sg>
2023-02-05 16:31:40 +0800

    Add sample test cases

--- Team05/Code05/src/unit_testing/src/PKB/PKBTest.cpp
+++ Team05/Code05/src/unit_testing/src/PKB/PKBTest.cpp
@@ -1,13 +1,14 @@
+#include <catch.hpp>
 #include <memory>
 #include <string>
 #include <utility>
 #include <vector>
-#include <catch.hpp>
 
 #include "PKB/PKBRead.h"
 #include "PKB/PKBRelationTable.h"
 #include "PKB/PKBWrite.h"
-#include "common/filter/filters/ModifiesFilter.h"
+#include "common/filter/filters/AssignFilter.h"
+#include "models/AST/factor_node/FactorNode.h"
 
 using namespace pkb;  // NOLINT
 
@@ -29,5 +30,23 @@ TEST_CASE("PKB read and write test") {
         pkb_read.Modifies(std::make_unique<filter::ModifiesFilterByLine>(10))
             ->get_result();
     REQUIRE(expected_table == *(result));
+  }
+
+  SECTION("PKB read and writes assign") {
+    std::unique_ptr<PKBRelationTable> table =
+        std::make_unique<PKBRelationTable>();
+    PKBWrite pkb_write(std::move(table));
+    pkb_write.AddAssignData("some", 10, std::make_unique<ast::ConstNode>(5));
+    IndexableTable<std::shared_ptr<AssignData>> expected_table;
+    expected_table.add_row(
+        10, std::make_shared<AssignData>("some", 10,
+                                         std::make_unique<ast::ConstNode>(5)));
+    table = pkb_write.EndWrite();
+
+    PKBRead pkb_read(std::move(table));
+    auto result = pkb_read.Assigns(std::make_unique<filter::AssignFilterAll>())
+                      ->get_result();
+    // unable to require as Assign Table is all references
+    // REQUIRE(expected_table == *(result));
   }
 }

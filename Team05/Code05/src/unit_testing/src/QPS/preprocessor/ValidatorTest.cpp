e3a695c16983156dd8783590c62ba570f24961c5
Halpfrog <62001923+Halpfrog@users.noreply.github.com>
2023-02-05 14:11:05 +0800

    Removed repeated use of BuildQuery

--- Team05/Code05/src/unit_testing/src/QPS/preprocessor/ValidatorTest.cpp
+++ Team05/Code05/src/unit_testing/src/QPS/preprocessor/ValidatorTest.cpp
@@ -2,22 +2,10 @@
 
 #include <QPS/preprocessor/Validator.h>
 #include <Common/Exceptions.h>
+#include <QPS/preprocessor/ParserTest.cpp>
 using namespace qps;
 void require(bool b) { REQUIRE(b);  }
 
-Query BuildQuery(
-    std::vector<std::pair<std::string, models::EntityStub>> synonyms,
-    std::vector<std::string> selected_synonyms) {
-  Query query;
-  for (auto [syn, entity] : synonyms) {
-    query.set_synonym(syn, entity);
-  }
-  for (auto selected_syn : selected_synonyms) {
-    query.add_selected_synonym(selected_syn);
-  }
-
-  return query;
-}
 TEST_CASE("Test isWildcard") { 
     Validator validator; 
   SECTION("Happy path");

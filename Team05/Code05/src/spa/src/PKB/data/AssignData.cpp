e836f11a333563378b9d859a45d7ca005a6f0b4c
aditi2313 <guptaaditi2313@gmail.com>
2023-02-06 00:27:04 +0800

    Checkstyle

--- Team05/Code05/src/spa/src/PKB/data/AssignData.cpp
+++ Team05/Code05/src/spa/src/PKB/data/AssignData.cpp
@@ -3,9 +3,10 @@
 #include <string>
 
 namespace pkb {
-    AssignData::AssignData(std::string variable, int line, std::unique_ptr<ast::ExprNode> expression) {
+    AssignData::AssignData(std::string variable, int line,
+                           std::unique_ptr<ast::ExprNode> expression) {
         variable_ = variable;
         line_ = line;
         expression_ = std::move(expression);
     }
-} // namespace pkb
+}  // namespace pkb
--- Team05/Code05/src/spa/src/common/filter/filters/AssignFilter.cpp
+++ Team05/Code05/src/spa/src/common/filter/filters/AssignFilter.cpp
@@ -10,18 +10,19 @@
 
 namespace filter {
 
-    AssignTablePtr filter::AssignFilterByVariable::FilterTable(AssignTablePtr assign) {
-        AssignTablePtr result = std::make_unique<pkb::AssignTable>();
+AssignTablePtr filter::AssignFilterByVariable::FilterTable
+    (AssignTablePtr assign) {AssignTablePtr result =
+    std::make_unique<pkb::AssignTable>();
 
-        for (int line : assign->get_indexes()) {
-            auto assign_data = assign->get_row(line);
-            std::string name = assign_data->get_variable();
-            if (variable_ == name) {
-                result->add_row(line, assign_data);
-                break;
-            }
+    for (int line : assign->get_indexes()) {
+        auto assign_data = assign->get_row(line);
+        std::string name = assign_data->get_variable();
+        if (variable_ == name) {
+            result->add_row(line, assign_data);
+            break;
         }
-
-        return std::move(result);
     }
+
+    return std::move(result);
+}
 }  // namespace filter

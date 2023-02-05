3bcea47491c9b6356600eac150aa3a65ca353c24
aizatazhar <aizat@Yeos-MacBook-Pro.local>
2023-02-05 10:07:37 +0800

    Implement Parser subcomponent

--- Team05/Code05/src/spa/src/SP/Parser.cpp
+++ Team05/Code05/src/spa/src/SP/Parser.cpp
@@ -1,13 +1,148 @@
-#include "Parser.h"
-
-#include <stdio.h>
-
 #include <iostream>
+#include <utility>
+#include <memory>
 #include <string>
-#include <vector>
 
-#include "models/AST/TNode.h"
+#include "models/AST/Token.h"
+#include "Parser.h"
+#include "Lexer.h"
 
 namespace sp {
-int Parse() { return 0; }
+Parser::Parser(std::unique_ptr<Lexer> lexer) : lexer_(std::move(lexer)) { }
+
+int current_tok_;
+int Parser::GetNextTok() {
+    return current_tok_ = lexer_->GetTok();
+}
+
+void Parser::MainLoop() {
+    while (true) {
+        if (current_tok_ == kTokProcedure) {
+            ParseProcedure();
+        } else {
+            GetNextTok();
+        }
+    }
+}
+
+void Parser::ParseProcedure() {
+    if (GetNextTok() != kTokIdent) {
+        throw std::runtime_error("procedure should be followed by a name");
+    }
+
+    std::cout << "procedure\n";
+
+    if (GetNextTok() != kTokOpenCurly) {
+        throw std::runtime_error("expected '{' after procedure name");
+    }
+
+    ParseStmtLst();
+
+    if (current_tok_ != kTokCloseCurly) {
+        throw std::runtime_error("expected '}' after procedure stmtLst");
+    }
+}
+
+void Parser::ParseStmtLst() {
+    if (GetNextTok() != kTokIdent) {
+        throw std::runtime_error("expected at least 1 stmt in stmtLst");
+    }
+
+    while (current_tok_ != kTokCloseCurly) {
+        ParseStmt();
+        GetNextTok();
+    }
+}
+
+void Parser::ParseStmt() {
+    if (current_tok_ != kTokIdent) {
+        throw std::runtime_error("expected stmt to begin with an ident_");
+    }
+
+    std::cout << "stmt\n";
+
+    std::string ident = lexer_->get_ident();
+    if (ident == "read") {
+        ParseRead();
+    } else if (ident == "print") {
+        ParsePrint();
+    } else if (ident == "call") {
+        ParseCall();
+    } else if (ident == "while") {
+        ParseWhile();
+    } else if (ident == "if") {
+        ParseIf();
+    } else {
+        ParseExpr();
+    }
+}
+
+void Parser::ParseRead() {
+    if (GetNextTok() != kTokIdent) {
+        throw std::runtime_error("read should be followed by a name");
+    }
+
+    std::cout << "read " + lexer_->get_ident() + "\n";
+
+    if (GetNextTok() != kTokSemicolon) {
+        throw std::runtime_error("expected end of a statement");
+    }
+}
+
+void Parser::ParsePrint() {
+    if (GetNextTok() != kTokIdent) {
+        throw std::runtime_error("print should be followed by a name");
+    }
+
+    std::cout << "print " + lexer_->get_ident() + "\n";
+
+    if (GetNextTok() != kTokSemicolon) {
+        throw std::runtime_error("expected end of a statement");
+    }
+}
+
+void Parser::ParseCall() {
+    if (GetNextTok() != kTokIdent) {
+        throw std::runtime_error("call should be followed by a name");
+    }
+
+    std::cout << "call " + lexer_->get_ident() + "\n";
+
+    if (GetNextTok() != kTokSemicolon) {
+        throw std::runtime_error("expected end of a statement");
+    }
+}
+
+void Parser::ParseWhile() {
+    // TODO(aizatazhar): implement
+}
+
+void Parser::ParseIf() {
+    // TODO(aizatazhar): implement
+}
+
+void Parser::ParseExpr() {
+    if (current_tok_ == kTokIdent) {
+        std::cout << lexer_->get_ident();
+    } else if (current_tok_ == kTokInteger) {
+        std::cout << lexer_->get_integer();
+    }
+
+    GetNextTok();
+
+    while (current_tok_ != kTokSemicolon) {
+        if (current_tok_ != kTokEquals && current_tok_ != kTokPlus
+            && current_tok_ != kTokMinus && current_tok_ != kTokTimes
+            && current_tok_ != kTokDiv && current_tok_ != kTokMod
+            && current_tok_ != kTokIdent && current_tok_ != kTokInteger) {
+            throw std::runtime_error("expected an expression");
+        }
+
+        std::cout << " " + std::to_string(current_tok_) + " ";
+
+        GetNextTok();  // eat the operator
+
+        ParseExpr();
+    }
+}
 }  // namespace sp
--- Team05/Code05/src/spa/src/SP/Parser.h
+++ Team05/Code05/src/spa/src/SP/Parser.h
@@ -1,4 +1,31 @@
 #pragma once
+
+#include "Lexer.h"
+#include <unordered_set>
+#include <memory>
+
 namespace sp {
-int Parse();
+class Parser {
+ public:
+    explicit Parser(std::unique_ptr<Lexer> lexer);
+
+    void MainLoop();
+
+ private:
+        std::unique_ptr<Lexer> lexer_;
+
+        int GetNextTok();
+
+        void ParseProcedure();
+
+        void ParseStmtLst();
+        void ParseStmt();
+
+        void ParseRead();
+        void ParsePrint();
+        void ParseCall();
+        void ParseWhile();
+        void ParseIf();
+        void ParseExpr();
+};
 }  // namespace sp

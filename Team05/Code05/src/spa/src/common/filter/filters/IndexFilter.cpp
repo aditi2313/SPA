6a98874bc2cf21c9a2d29c75d485940570d68b30
aditi2313 <guptaaditi2313@gmail.com>
2023-02-12 01:19:28 +0800

    IndexFilter

---
+++ Team05/Code05/src/spa/src/common/filter/filters/IndexFilter.cpp
@@ -0,0 +1,12 @@
+#include "IndexFilter.h"
+
+namespace filter {
+
+template<class T>
+pkb::IndexableTablePtr<T> IndexFilter<T>::FilterTable(pkb::IndexableTablePtr<T> table) {
+    pkb::IndexableTablePtr<T> result = std::make_unique<pkb::IndexableTable<T>>();
+    auto row = table->get_row(line_);
+    result->add_row(row->get_line(), row);
+    return result;
+}
+}  // namespace pkb

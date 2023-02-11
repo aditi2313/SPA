d504bb12fce95eef1a2176cf3a1583328dd77fd4
aditi2313 <guptaaditi2313@gmail.com>
2023-02-12 01:19:28 +0800

    IndexFilter

---
+++ Team05/Code05/src/spa/src/common/filter/filters/IndexFilter.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "IndexableFilter.h"
+#include "PKB/data/AssignData.h"
+#include "PKB/tables/IndexableTable.h"
+#include "common/Exceptions.h"
+
+namespace filter {
+
+template<class T>
+class IndexFilter
+        : public IndexableFilter<T> {
+public:
+    explicit IndexFilter(int line) : line_(line) {}
+
+    pkb::IndexableTablePtr<T> FilterTable(pkb::IndexableTablePtr<T>) override;
+
+private:
+    int line_;
+};
+}

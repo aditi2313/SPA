8583d20d09a86213c3529b4b3fb59810c9042fb2
aditi2313 <guptaaditi2313@gmail.com>
2023-02-13 00:25:51 +0800

    Checkstyle

--- Team05/Code05/src/spa/src/common/filter/filters/IndexFilter.cpp
+++ Team05/Code05/src/spa/src/common/filter/filters/IndexFilter.cpp
@@ -3,8 +3,10 @@
 namespace filter {
 
 template<class T>
-pkb::IndexableTablePtr<T> IndexFilter<T>::FilterTable(pkb::IndexableTablePtr<T> table) {
-    pkb::IndexableTablePtr<T> result = std::make_unique<pkb::IndexableTable<T>>();
+pkb::IndexableTablePtr<T> IndexFilter<T>::FilterTable
+(pkb::IndexableTablePtr<T> table) {
+    pkb::IndexableTablePtr<T> result =
+            std::make_unique<pkb::IndexableTable<T>>();
     auto row = table->get_row(line_);
     result->add_row(row->get_line(), row);
     return result;
--- Team05/Code05/src/spa/src/common/filter/filters/PredicateFilter.cpp
+++ Team05/Code05/src/spa/src/common/filter/filters/PredicateFilter.cpp
@@ -3,8 +3,10 @@
 namespace filter {
 
 template<class T, typename Predicate>
-pkb::IndexableTablePtr<T> PredicateFilter<T, Predicate>::FilterTable(pkb::IndexableTablePtr<T> table) {
-    pkb::IndexableTablePtr<T> result = std::make_unique<pkb::IndexableTable<T>>();
+pkb::IndexableTablePtr<T> PredicateFilter<T, Predicate>::FilterTable
+(pkb::IndexableTablePtr<T> table) {
+    pkb::IndexableTablePtr<T> result =
+            std::make_unique<pkb::IndexableTable<T>>();
 
     for (int line : table->get_indexes()) {
         auto data = table->get_row(line);

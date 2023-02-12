a9763035cbd7fd7169c66521815f9ad34e1a70a4
aditi2313 <guptaaditi2313@gmail.com>
2023-02-12 20:23:00 +0800

    PredicateFilter

---
+++ Team05/Code05/src/spa/src/common/filter/filters/PredicateFilter.cpp
@@ -0,0 +1,18 @@
+#include "PredicateFilter.h"
+
+namespace filter {
+
+template<class T, typename Predicate>
+pkb::IndexableTablePtr<T> PredicateFilter<T, Predicate>::FilterTable(pkb::IndexableTablePtr<T> table) {
+    pkb::IndexableTablePtr<T> result = std::make_unique<pkb::IndexableTable<T>>();
+
+    for (int line : table->get_indexes()) {
+        auto data = table->get_row(line);
+        if (predicate_(data)) {
+            result->add_row(line, data);
+            break;
+        }
+    }
+    return std::move(result);
+}
+}  // namespace pkb
---
+++ Team05/Code05/src/spa/src/common/filter/filters/PredicateFilter.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "IndexableFilter.h"
+#include "PKB/data/AssignData.h"
+#include "PKB/tables/IndexableTable.h"
+#include "common/Exceptions.h"
+
+namespace filter {
+
+    template<class T, typename Predicate>
+    class PredicateFilter
+            : public IndexableFilter<T> {
+    public:
+        explicit PredicateFilter(Predicate predicate) : predicate_(predicate) {}
+
+        pkb::IndexableTablePtr<T> FilterTable(pkb::IndexableTablePtr<T>) override;
+
+    private:
+        Predicate predicate_;
+    };
+}  // namespace pkb

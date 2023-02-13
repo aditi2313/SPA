1a5835d1c3aea912b269c35eda5013e7f7eee82f
aditi2313 <guptaaditi2313@gmail.com>
2023-02-13 13:45:17 +0800

    Inline functions

--- Team05/Code05/src/spa/src/common/filter/filters/IndexFilter.h
+++ Team05/Code05/src/spa/src/common/filter/filters/IndexFilter.h
@@ -23,13 +23,11 @@ class IndexFilter
             FilterTable(pkb::IndexableTablePtr<T> table) override {
         pkb::IndexableTablePtr<T> result =
                 std::make_unique<pkb::IndexableTable<T>>();
-        auto indexes = table->get_indexes();
-        if (indexes.find(line_) == indexes.end()) {
+        if (table->exists(line_)) {
+            auto row = table->get_row(line_);
+            result->add_row(row->get_line(), row);
             return result;
         }
-        auto row = table->get_row(line_);
-        result->add_row(row->get_line(), row);
-        return result;
     }
 
  private:

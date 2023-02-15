c38f4ccf3617ae76362cfcecde2ad6702bff58ed
Au Chen Xi, Gabriel Au <e0544006@u.nus.edu>
2023-02-15 16:48:07 +0800

    Fix checkstyle

--- Team05/Code05/src/spa/src/models/AST/Token.cpp
+++ Team05/Code05/src/spa/src/models/AST/Token.cpp
@@ -1,10 +1,12 @@
 #include "Token.h"
 
+#include <vector>
+
 namespace sp {
 const std::vector<Token> kStmtTokens{Token::kTokRead, Token::kTokPrint,
                                      Token::kTokCall, Token::kTokWhile,
                                      Token::kTokIf};
-bool IsStmtToken(Token input) { 
+bool IsStmtToken(Token input) {
   for (const auto& token : kStmtTokens) {
     if (token == input) {
       return true;

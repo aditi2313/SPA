a1a211b254d88bf03f477d147cd71feef65e0503
aizatazhar <aizat@Yeos-MacBook-Pro.local>
2023-02-17 00:35:52 +0800

    Fix StatementParser

--- Team05/Code05/src/spa/src/SP/parser/stmts/CallParser.h
+++ Team05/Code05/src/spa/src/SP/parser/stmts/CallParser.h
@@ -13,18 +13,14 @@ namespace sp {
 class CallParser : Parser<ast::CallNode> {
  public:
   std::unique_ptr<ast::CallNode> parse(Lexer& lxr) override {
-    if (lxr.GetTokAndIncrement() != Token::kTokIdent) {
-      // TODO(aizatazhar): use custom exception
-      throw std::runtime_error("call should be followed by a name");
-    }
+    AssertExpectedToken(lxr.GetTokAndIncrement(), Token::kTokCall, "call");
+    AssertExpectedToken(lxr.GetTokAndIncrement(), Token::kTokIdent, "variable");
 
     std::string var_name = lxr.get_ident();
     std::cout << "call " + lxr.get_ident() << std::endl;
 
-    if (lxr.GetTokAndIncrement() != Token::kTokSemicolon) {
-      // TODO(aizatazhar): use custom exception
-      throw std::runtime_error("expected end of a statement");
-    }
+    AssertExpectedToken(lxr.GetTokAndIncrement(), Token::kTokSemicolon, ";");
+
     auto var_node = std::make_unique<ast::VarNode>(ast::VarNode(var_name));
     return std::make_unique<ast::CallNode>(std::move(var_node),
                                            lxr.GetAndIncrementStmtCtr());

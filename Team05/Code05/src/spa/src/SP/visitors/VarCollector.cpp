7987326101dc9fd4210aba3ac08318bb0f02b536
nhjryan <e0544583@u.nus.edu>
2023-02-18 20:37:08 +0800

    Fix build issues

--- Team05/Code05/src/spa/src/SP/visitors/VarCollector.cpp
+++ Team05/Code05/src/spa/src/SP/visitors/VarCollector.cpp
@@ -4,29 +4,4 @@ namespace sp {
 void VarCollector::VisitVar(ast::VarNode* var_node) {
   vars.insert(var_node->get_name());
 }
-
-void VarCollector::VisitPlus(ast::PlusNode* plus_node) {
-  plus_node->get_expr()->AcceptVisitor(this);
-  plus_node->get_term()->AcceptVisitor(this);
-}
-
-void VarCollector::VisitMinus(ast::MinusNode* minus_node) {
-  minus_node->get_expr()->AcceptVisitor(this);
-  minus_node->get_term()->AcceptVisitor(this);
-}
-
-void VarCollector::VisitTimes(ast::TimesNode* times_node) {
-  times_node->get_factor()->AcceptVisitor(this);
-  times_node->get_term()->AcceptVisitor(this);
-}
-
-void VarCollector::VisitDiv(ast::DivNode* div_node) {
-  div_node->get_factor()->AcceptVisitor(this);
-  div_node->get_term()->AcceptVisitor(this);
-}
-
-void VarCollector::VisitMod(ast::ModNode* mod_node) {
-  mod_node->get_factor()->AcceptVisitor(this);
-  mod_node->get_term()->AcceptVisitor(this);
-}
 }  // namespace sp
--- Team05/Code05/src/spa/src/SP/visitors/VarCollector.h
+++ Team05/Code05/src/spa/src/SP/visitors/VarCollector.h
@@ -13,16 +13,6 @@ class VarCollector : public TNodeVisitor {
  public:
   void VisitVar(ast::VarNode* var_node) override;
 
-  void VisitPlus(ast::PlusNode* plus_node) override;
-
-  void VisitMinus(ast::MinusNode* minus_node) override;
-
-  void VisitTimes(ast::TimesNode* times_node) override;
-
-  void VisitDiv(ast::DivNode* div_node) override;
-
-  void VisitMod(ast::ModNode* mod_node) override;
-
   std::unordered_set<std::string> get_vars() { return vars; }
 
  private:

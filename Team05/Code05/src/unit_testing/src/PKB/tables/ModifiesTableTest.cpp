49466c1e16202f58b3d67ff6fe5380bc793d727e
aditi2313 <guptaaditi2313@gmail.com>
2023-02-19 15:02:31 +0800

    Modifies and Uses test additions.

--- Team05/Code05/src/unit_testing/src/PKB/tables/ModifiesTableTest.cpp
+++ Team05/Code05/src/unit_testing/src/PKB/tables/ModifiesTableTest.cpp
@@ -8,14 +8,30 @@
 using namespace pkb;  // NOLINT
 
 TEST_CASE("Test ModifiesTable") {
-  ModifiesTable modifies_table;
-  std::unordered_set<std::string> variables{"a"};
-  ModifiesData modifies_data(10, variables);
-  modifies_table.add_row(5, modifies_data);
+    std::unordered_set<std::string> variables1{"a", "b", "c"};
+    std::unordered_set<std::string> variables2{"d", "e", "f"};
+    ModifiesData modifies_data1(1, variables1);
+    ModifiesData modifies_data2(3, variables2);
 
-  SECTION("Retrieving ModifiesTable row") {
-    ModifiesData retrieved_modifies_data = modifies_table.get_row(5);
-    REQUIRE(((retrieved_modifies_data.get_line() == 10) &&
-        retrieved_modifies_data.get_variables().count("a")));
-  };
+    ModifiesTable modifies_table1;
+    modifies_table1.add_row(5, modifies_data1);
+    ModifiesTable modifies_table2;
+    modifies_table2.add_row(5, modifies_data2);
+
+    SECTION("Checking if ModifiesTable rows exists") {
+        REQUIRE((modifies_table1.exists(5) && modifies_table2.exists(5)));
+    };
+
+    SECTION("Checking if ModifiesTable row is empty") {
+        REQUIRE(!(modifies_table1.empty() && modifies_table2.empty()));
+    };
+
+    SECTION("Retrieving ModifiesTable row") {
+        ModifiesData retrieved_modifies_data = modifies_table1.get_row(5);
+        REQUIRE(retrieved_modifies_data == modifies_data1);
+    };
+
+    SECTION("Testing ModifiesTable equality") {
+        REQUIRE(!(modifies_table1 == modifies_table2));
+    };
 }
--- Team05/Code05/src/unit_testing/src/PKB/tables/UsesTableTest.cpp
+++ Team05/Code05/src/unit_testing/src/PKB/tables/UsesTableTest.cpp
@@ -8,14 +8,30 @@
 using namespace pkb;  // NOLINT
 
 TEST_CASE("Test UsesTable") {
-    UsesTable uses_table;
-    std::unordered_set<std::string> variables{"a"};
-    UsesData uses_data(10, variables);
-    uses_table.add_row(5, uses_data);
+    std::unordered_set<std::string> variables1{"a", "b", "c"};
+    std::unordered_set<std::string> variables2{"d", "e", "f"};
+    UsesData uses_data1(1, variables1);
+    UsesData uses_data2(3, variables2);
+
+    UsesTable uses_table1;
+    uses_table1.add_row(5, uses_data1);
+    UsesTable uses_table2;
+    uses_table2.add_row(5, uses_data2);
+
+    SECTION("Checking if UsesTable rows exists") {
+        REQUIRE((uses_table1.exists(5) && uses_table2.exists(5)));
+    };
+
+    SECTION("Checking if UsesTable row is empty") {
+        REQUIRE(!(uses_table1.empty() && uses_table2.empty()));
+    };
 
     SECTION("Retrieving UsesTable row") {
-        UsesData retrieved_uses_data = uses_table.get_row(5);
-        REQUIRE(((retrieved_uses_data.get_line() == 10) &&
-                retrieved_uses_data.get_variables().count("a")));
+        UsesData retrieved_uses_data = uses_table1.get_row(5);
+        REQUIRE(retrieved_uses_data == uses_data1);
+    };
+
+    SECTION("Testing UsesTable equality") {
+        REQUIRE(!(uses_table1 == uses_table2));
     };
 }

589296d28a97ca6ac0df5620848feeb818185894
okg00dbye <E0425498@u.nus.edu>
2023-02-25 19:14:21 +0800

    Undo formatting changes

--- Team05/Code05/src/spa/src/PKB/data/UsesData.h
+++ Team05/Code05/src/spa/src/PKB/data/UsesData.h
@@ -8,29 +8,29 @@ namespace pkb {
 
 class UsesData {
  public:
-  UsesData(int line, const std::unordered_set<std::string>& variable_names);
-  friend bool operator<(const UsesData& LHS, const UsesData& RHS) {
-    return LHS.line_ < RHS.line_ ||
-        (LHS.line_ == RHS.line_ &&
-            std::lexicographical_compare(LHS.variable_names_.begin(),
-                                         LHS.variable_names_.end(),
-                                         RHS.variable_names_.begin(),
-                                         RHS.variable_names_.end()));
-  }
+    UsesData(int line, const std::unordered_set<std::string>& variable_names);
+    friend bool operator<(const UsesData& LHS, const UsesData& RHS) {
+        return LHS.line_ < RHS.line_ ||
+            (LHS.line_ == RHS.line_ &&
+                std::lexicographical_compare(LHS.variable_names_.begin(),
+                                           LHS.variable_names_.end(),
+                                           RHS.variable_names_.begin(),
+                                           RHS.variable_names_.end()));
+    }
 
-  friend bool operator==(const UsesData& LHS, const UsesData& RHS) {
-    return LHS.line_ == RHS.line_ &&
-        LHS.variable_names_ == RHS.variable_names_;
-  }
+    friend bool operator==(const UsesData& LHS, const UsesData& RHS) {
+        return LHS.line_ == RHS.line_ &&
+            LHS.variable_names_ == RHS.variable_names_;
+    }
 
-  inline int get_line() { return line_; }
+    inline int get_line() { return line_; }
 
-  inline const std::unordered_set<std::string>& get_variables() {
-    return variable_names_;
-  }
+    inline const std::unordered_set<std::string>& get_variables() {
+        return variable_names_;
+    }
 
  private:
-  int line_;
-  std::unordered_set<std::string> variable_names_;
+    int line_;
+    std::unordered_set<std::string> variable_names_;
 };
 }  // namespace pkb

5d7afb3a336f269f8d750d4aa97d3cce3f3b5163
aditi2313 <guptaaditi2313@gmail.com>
2023-02-26 23:19:12 +0800

    Checkstyle

--- Team05/Code05/src/spa/src/PKB/data/CallsData.cpp
+++ Team05/Code05/src/spa/src/PKB/data/CallsData.cpp
@@ -2,7 +2,7 @@
 
 namespace pkb {
     pkb::CallsData::CallsData(std::string caller, std::string callee)
-            : caller_(caller), callee_(callee){
+            : caller_(caller), callee_(callee) {
         callee_list_.insert(callee);
     }
 }  // namespace pkb

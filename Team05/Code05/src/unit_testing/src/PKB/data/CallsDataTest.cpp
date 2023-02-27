bf70ecae62f69af0c8bca3e62b6b3de9f1fbc6f6
aditi2313 <guptaaditi2313@gmail.com>
2023-02-27 22:05:23 +0800

    NextDataTest and CallsDataTest.

---
+++ Team05/Code05/src/unit_testing/src/PKB/data/CallsDataTest.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <catch.hpp>
+
+#include "PKB/data/CallsData.h"
+
+using namespace pkb; // NOLINT
+
+TEST_CASE("Test CallsData") {
+    CallsData calls_data("caller", "callee1");
+
+    SECTION("Retrieving CallsData caller") {
+        REQUIRE(calls_data.get_caller() == "caller");
+    };
+
+    SECTION("Retrieving CallsData callee") {
+        REQUIRE(calls_data.get_callee() == "callee1");
+    };
+
+    SECTION("Retrieving CallsData callee_list") {
+        calls_data.add_to_list("callee2");
+        std::unordered_set<std::string> expected_callee_list{"callee1", "callee2"};
+        REQUIRE(calls_data.get_callee_list() == expected_callee_list);
+    };
+}
---
+++ Team05/Code05/src/unit_testing/src/PKB/data/NextDataTest.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <catch.hpp>
+
+#include "PKB/data/NextData.h"
+
+using namespace pkb; // NOLINT
+
+TEST_CASE("Test NextData") {
+    NextData next_data(1, 2);
+
+    SECTION("Retrieving NextData line") {
+        REQUIRE(next_data.get_line() == 1);
+    };
+
+    SECTION("Retrieving NextData next line") {
+        REQUIRE(next_data.get_next() == 2);
+    };
+
+    SECTION("Retrieving NextData callee_list") {
+        next_data.add_to_list(3);
+        std::unordered_set<int> expected_next_list{2, 3};
+        REQUIRE(next_data.get_next_list() == expected_next_list);
+    };
+}

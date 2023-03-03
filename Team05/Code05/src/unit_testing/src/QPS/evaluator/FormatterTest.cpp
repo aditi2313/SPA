2c1f43e8a4587ec76e157e63c6b9fdffd84fd75d
okg00dbye <E0425498@u.nus.edu>
2023-03-03 14:23:26 +0800

    Add unit test for formatting for tuples

--- Team05/Code05/src/unit_testing/src/QPS/evaluator/FormatterTest.cpp
+++ Team05/Code05/src/unit_testing/src/QPS/evaluator/FormatterTest.cpp
@@ -10,7 +10,7 @@ using namespace models;  // NOLINT
 TEST_CASE("Test QueryFormatter") {
   Formatter query_formatter;
 
-  SECTION("For statements") {
+  SECTION("Single selected synonym") {
     EntitySet entities;
     entities.insert(Entity(1));
     std::vector<EntitySet> selected_entities = {entities};
@@ -19,5 +19,17 @@ TEST_CASE("Test QueryFormatter") {
     std::list<std::string> actual =
         query_formatter.FormatQuery(query_result);
     REQUIRE(actual.front() == "1");
+  };
+
+  SECTION("Multiple selected synonyms") {
+    EntitySet entities1, entities2;
+    entities1.insert(Entity(1));
+    entities2.insert(Entity(2));
+    std::vector<EntitySet> selected_entities = {entities1, entities2};
+    QueryResultPtr query_result = std::make_unique<ListQueryResult>(
+        selected_entities);
+    std::list<std::string> actual =
+        query_formatter.FormatQuery(query_result);
+    REQUIRE(actual.front() == "1, 2");
   };
 }

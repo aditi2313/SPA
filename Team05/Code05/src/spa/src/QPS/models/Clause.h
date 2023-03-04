8148a769cb92ac51d78567ec37f1a14958602a7f
okg00dbye <E0425498@u.nus.edu>
2023-03-04 19:28:33 +0800

    Create CallsClause

--- Team05/Code05/src/spa/src/QPS/models/Clause.h
+++ Team05/Code05/src/spa/src/QPS/models/Clause.h
@@ -184,6 +184,18 @@ class ParentTClause : public Clause {
              EntitySet &results) override;
 };
 
+class CallsClause : public Clause {
+ public:
+  CallsClause(ArgumentPtr arg1, ArgumentPtr arg2)
+      : Clause(std::move(arg1), std::move(arg2)) {
+    rel_name_ = PQL::kCallsRelName;
+  }
+
+  void Index(const Entity &index,
+             const pkb::PKBReadPtr &pkb,
+             EntitySet &results) override;
+};
+
 using ClausePtr = std::unique_ptr<Clause>;
 
 }  // namespace qps

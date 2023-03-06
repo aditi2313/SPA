7f19026aeb9eb29e0e4b111fccb89ce6330221c7
aizatazhar <aizat.azharr@gmail.com>
2023-03-06 22:51:55 +0800

    Implement ModifiesP in ModifiesVisitor

--- Team05/Code05/src/spa/src/SP/visitors/ModifiesVisitor.h
+++ Team05/Code05/src/spa/src/SP/visitors/ModifiesVisitor.h
@@ -18,12 +18,15 @@ class ModifiesVisitor : public PKBWritingVisitor {
   explicit ModifiesVisitor(std::unique_ptr<pkb::PKBWrite>&& pkb_ptr)
       : PKBWritingVisitor(std::move(pkb_ptr)) {}
 
+  void ProcessAfter(ast::ProgramNode* program_node) override;
+
+  void Process(ast::ProcNode* proc_node) override;
+
   void Process(ast::AssignNode* assign_node) override;
 
   void Process(ast::ReadNode* read_node) override;
 
-  // TODO(Gab) Go into call node and get the information #41
-  void Process(ast::CallNode* call_node) override {}
+  void Process(ast::CallNode* call_node) override;
 
   void ProcessAft(ast::IfNode*) override;
   void ProcessAft(ast::WhileNode*) override;
@@ -48,5 +51,14 @@ class ModifiesVisitor : public PKBWritingVisitor {
     pkb = reader.EndRead();
     pkb_ptr_ = std::make_unique<pkb::PKBWrite>(std::move(pkb));
   }
+
+  // Mapping from a procedure to the variables it modifies directly
+  std::unordered_map<std::string, std::unordered_set<std::string>>
+      direct_modifies_;
+  // Mapping from a procedure to the procedures which call it
+  std::unordered_map<std::string, std::unordered_set<std::string>> called_by_;
+  // Mapping from a procedure to the calls within the procedure
+  std::unordered_map<std::string, std::unordered_set<std::string>> proc_calls_;
+  std::string current_procedure_;
 };
 }  // namespace sp

c252902cdb6ed4be88b4d52d7f5f0e050038d46d
Au Chen Xi, Gabriel <au.gabriel@yahoo.com.sg>
2023-03-06 20:43:57 +0800

    Add sections to the logger

--- Team05/Code05/src/spa/src/SP/SourceProcessor.h
+++ Team05/Code05/src/spa/src/SP/SourceProcessor.h
@@ -33,7 +33,7 @@ class SourceProcessor {
   }
 
   static std::unique_ptr<ast::ProgramNode> ParseProgram(std::string program) {
-    logging::Logger::Start();
+    logging::Logger::ResetClock();
     sp::Lexer lxr(std::move(program));
     sp::ProgramParser program_parser;
     auto root = program_parser.parse(lxr);
@@ -47,66 +47,57 @@ class SourceProcessor {
 
   static void ExtractRelationships(
       std::unique_ptr<ast::ProgramNode> &root,
-      std::unique_ptr<pkb::PKBRelationTable> &pkb_relation) {
-    logging::Logger::Start();
+      std::unique_ptr<pkb::PKBRelationTable> &pkb_relation) {    
     auto writer = std::make_unique<pkb::PKBWrite>(std::move(pkb_relation));
-
-    logging::Logger::Start();
+    logging::Logger::EnterSection("Extract Relationships");
     sp::AssignVisitor av(std::move(writer));
     root->AcceptVisitor(&av);
     writer = av.EndVisit();
     logging::Logger::LogAndStop("Assign visitor finished");
-
-    logging::Logger::Start();
+    
     sp::DataVisitor dv(std::move(writer));
     root->AcceptVisitor(&dv);
     writer = dv.EndVisit();
     logging::Logger::LogAndStop("Data visitor finished");
-
-    logging::Logger::Start();
+    
     sp::ModifiesVisitor mv(std::move(writer));
     root->AcceptVisitor(&mv);
     writer = mv.EndVisit();
     logging::Logger::LogAndStop("Modifies visitor finished");
-
-    logging::Logger::Start();
+    
     sp::ParentVisitor pv(std::move(writer));
     root->AcceptVisitor(&pv);
     writer = pv.EndVisit();
     logging::Logger::LogAndStop("Parent visitor finished");
-
-    logging::Logger::Start();
+    
     sp::UsesVisitor uv(std::move(writer));
     root->AcceptVisitor(&uv);
     writer = uv.EndVisit();
     logging::Logger::LogAndStop("Uses visitor finished");
-
-    logging::Logger::Start();
+    
     sp::FollowsVisitor fv(std::move(writer));
     root->AcceptVisitor(&fv);
     writer = fv.EndVisit();
     logging::Logger::LogAndStop("Follows visitor finished");
-
-    logging::Logger::Start();
+    
     sp::CallsVisitor cv(std::move(writer));
     root->AcceptVisitor(&cv);
     writer = cv.EndVisit();
     logging::Logger::LogAndStop("Calls visitor finished");
-
-    logging::Logger::Start();
+    
     sp::CFGGeneratingVisitor cfg_generator;
     cfg_generator.VisitProgram(root.get());
     auto cfg = cfg_generator.CreateCFG();
     logging::Logger::LogAndStop("Cfg generation finished");
-
-    logging::Logger::Start();
+    
     sp::CFGExtractor cfg_extractor(std::move(writer));
     cfg_extractor.WriteCFG(*cfg);
     writer = cfg_extractor.EndVisit();
     logging::Logger::LogAndStop("Cfg extraction finished");
 
     pkb_relation = writer->ProcessTableAndEndWrite();
-    logging::Logger::LogAndStop("End pkb writing");
+    logging::Logger::LogAndStop("End pkb processing");
+    logging::Logger::ExitSection("Extract Relationships");
   }
 };
 
--- Team05/Code05/src/spa/src/common/logging/Logger.h
+++ Team05/Code05/src/spa/src/common/logging/Logger.h
@@ -14,23 +14,51 @@ namespace logging {
 /// </summary>
 class Logger {
  public:
-  static void Start() {
-    if (disabled) return;
+  static void ResetClock() {
+    if (disabled_) return;
+    if (!times_.empty()) times_.pop();
     times_.push(Clock::now());
   }
 
   // TODO(Gab) consider removal of desc as it creates unnecessary overhead
+  
+  /// <summary>
+  /// Function writes the interval from the previous log,
+  /// or the previous reset, or the start of the current section
+  /// as well as the provided description into the log.
+  /// </summary>
+  /// <param name="desc"></param>
   static void LogAndStop(std::string desc) {
-    if (disabled) return;
+    if (disabled_) return;
+    Clock::time_point curr = Clock::now();
+    std::chrono::duration<double> interval = curr - times_.top();
+    times_.pop();
+    times_.push(curr);
+    std::cout << desc << " time: " << interval.count() << " s" << std::endl;
+  }
+
+  static void EnterSection(std::string desc) {
+    if (disabled_) return;
+    times_.push(Clock::now());
+    times_.push(Clock::now());
+    std::cout << kSection << "Enter Section: " << desc << kSection << std::endl;
+  }
+
+  static void ExitSection(std::string desc) {
+    if (disabled_) return;
+    times_.pop();
     auto curr = Clock::now();
     std::chrono::duration<double> interval = curr - times_.top();
-    std::cout << desc << " time: " << interval.count() << " s" << std::endl;
     times_.pop();
+    std::cout << kSection << "Exit Section: " << desc
+              << " With interval: " << interval.count() << kSection
+              << std::endl;
   }
 
  private:
-  inline static bool disabled = false;
+  inline static bool disabled_ = false;
   inline static std::stack<Clock::time_point> times_;
+  inline static const char kSection[] = " <===========================> ";
 };
 
 }  // namespace logging

e45b6aeb3cac89e6ef8f77f6856471eac11889c7
nhjryan <e0544583@u.nus.edu>
2023-03-08 20:41:01 +0800

    Finish ParentTest

--- Team05/Code05/src/integration_testing/src/SP_PKB/ParentTest.cpp
+++ Team05/Code05/src/integration_testing/src/SP_PKB/ParentTest.cpp
@@ -35,7 +35,7 @@ pkb::ParentTable InitializeEmptyExpectedResult() {
 
 TEST_CASE("Test SP and PKB integration for Parent Data") {
   SECTION("One statement - empty table") {
-    std::string program = "procedure follows { read x; }";
+    std::string program = "procedure parent { read x; }";
 
     auto actual_results = InitializeActualResult(program);
 
@@ -45,7 +45,7 @@ TEST_CASE("Test SP and PKB integration for Parent Data") {
   }
 
   SECTION("Two read statements - empty table") {
-    std::string program = "procedure follows { read x; read y; }";
+    std::string program = "procedure parent { read x; read y; }";
 
     auto actual_results = InitializeActualResult(program);
 
@@ -55,7 +55,7 @@ TEST_CASE("Test SP and PKB integration for Parent Data") {
   }
 
   SECTION("While loop - relationship holds") {
-    std::string program = "procedure follows { while (x != 0) { y = y + 1; } }";
+    std::string program = "procedure parent { while (x != 0) { y = y + 1; } }";
 
     auto actual_results = InitializeActualResult(program);
 
@@ -63,6 +63,144 @@ TEST_CASE("Test SP and PKB integration for Parent Data") {
         std::make_unique<pkb::PKBRelationTable>();
     pkb::PKBWrite expected_writer(std::move(expected_table));
     expected_writer.AddParentData(1, 2);
+    expected_table = expected_writer.ProcessTableAndEndWrite();
+    pkb::PKBRead expected_reader(std::move(expected_table));
+    auto expected_ftr = std::make_unique<filter::ParentPredicateFilter>(
+        [](pkb::ParentData data) { return true; });
+    auto expected_results_ptr = expected_reader.Parent(std::move(expected_ftr));
+    auto expected_results = *(expected_results_ptr->get_result());
+
+    REQUIRE(actual_results == expected_results);
+  }
+
+  SECTION("If statement - relationship holds for then and else stmtlsts") {
+    std::string program =
+        "procedure parent { if (x == 0) then { read x; } else { read y; } }";
+
+    auto actual_results = InitializeActualResult(program);
+
+    std::unique_ptr<pkb::PKBRelationTable> expected_table =
+        std::make_unique<pkb::PKBRelationTable>();
+    pkb::PKBWrite expected_writer(std::move(expected_table));
+    expected_writer.AddParentData(1, 2);
+    expected_writer.AddParentData(1, 3);
+    expected_table = expected_writer.ProcessTableAndEndWrite();
+    pkb::PKBRead expected_reader(std::move(expected_table));
+    auto expected_ftr = std::make_unique<filter::ParentPredicateFilter>(
+        [](pkb::ParentData data) { return true; });
+    auto expected_results_ptr = expected_reader.Parent(std::move(expected_ftr));
+    auto expected_results = *(expected_results_ptr->get_result());
+
+    REQUIRE(actual_results == expected_results);
+  }
+
+  SECTION("Double nested while loop - transitive relationship holds") {
+    std::string program =
+        "procedure parent { while (y > 5) { while (x < 3) { read z; } } }";
+
+    auto actual_results = InitializeActualResult(program);
+
+    std::unique_ptr<pkb::PKBRelationTable> expected_table =
+        std::make_unique<pkb::PKBRelationTable>();
+    pkb::PKBWrite expected_writer(std::move(expected_table));
+    expected_writer.AddParentData(1, 2);
+    expected_writer.AddParentData(2, 3);
+    expected_table = expected_writer.ProcessTableAndEndWrite();
+    pkb::PKBRead expected_reader(std::move(expected_table));
+    auto expected_ftr = std::make_unique<filter::ParentPredicateFilter>(
+        [](pkb::ParentData data) { return true; });
+    auto expected_results_ptr = expected_reader.Parent(std::move(expected_ftr));
+    auto expected_results = *(expected_results_ptr->get_result());
+
+    REQUIRE(actual_results == expected_results);
+  }
+
+  SECTION("Triple nested while loop - transitive relationship holds") {
+    std::string program =
+        "procedure parent { while (x > 3) { while (x < 3) { while (x == 3) { "
+        "read while; } } } }";
+
+    auto actual_results = InitializeActualResult(program);
+
+    std::unique_ptr<pkb::PKBRelationTable> expected_table =
+        std::make_unique<pkb::PKBRelationTable>();
+    pkb::PKBWrite expected_writer(std::move(expected_table));
+    expected_writer.AddParentData(1, 2);
+    expected_writer.AddParentData(2, 3);
+    expected_writer.AddParentData(3, 4);
+    expected_table = expected_writer.ProcessTableAndEndWrite();
+    pkb::PKBRead expected_reader(std::move(expected_table));
+    auto expected_ftr = std::make_unique<filter::ParentPredicateFilter>(
+        [](pkb::ParentData data) { return true; });
+    auto expected_results_ptr = expected_reader.Parent(std::move(expected_ftr));
+    auto expected_results = *(expected_results_ptr->get_result());
+
+    REQUIRE(actual_results == expected_results);
+  }
+
+  SECTION("Double nested if statement - transitive relationship holds") {
+    std::string program =
+        "procedure parent { if (x > 5) then { if (z > 5) then { read y; } else "
+        "{ read x; } } else { if (y > 5) then { read x; } else { read y; } } }";
+
+    auto actual_results = InitializeActualResult(program);
+
+    std::unique_ptr<pkb::PKBRelationTable> expected_table =
+        std::make_unique<pkb::PKBRelationTable>();
+    pkb::PKBWrite expected_writer(std::move(expected_table));
+    expected_writer.AddParentData(1, 2);
+    expected_writer.AddParentData(1, 5);
+    expected_writer.AddParentData(2, 3);
+    expected_writer.AddParentData(2, 4);
+    expected_writer.AddParentData(5, 6);
+    expected_writer.AddParentData(5, 7);
+    expected_table = expected_writer.ProcessTableAndEndWrite();
+    pkb::PKBRead expected_reader(std::move(expected_table));
+    auto expected_ftr = std::make_unique<filter::ParentPredicateFilter>(
+        [](pkb::ParentData data) { return true; });
+    auto expected_results_ptr = expected_reader.Parent(std::move(expected_ftr));
+    auto expected_results = *(expected_results_ptr->get_result());
+
+    REQUIRE(actual_results == expected_results);
+  }
+
+  SECTION("Two loops within a loop - relationship holds only in same branch") {
+    std::string program =
+        "procedure parent { while (y == 2) { if (x == 2) then { read x; } else "
+        "{ read y; } while (x > 2) { read z; } } }";
+
+    auto actual_results = InitializeActualResult(program);
+
+    std::unique_ptr<pkb::PKBRelationTable> expected_table =
+        std::make_unique<pkb::PKBRelationTable>();
+    pkb::PKBWrite expected_writer(std::move(expected_table));
+    expected_writer.AddParentData(1, 2);
+    expected_writer.AddParentData(1, 5);
+    expected_writer.AddParentData(2, 3);
+    expected_writer.AddParentData(2, 4);
+    expected_writer.AddParentData(5, 6);
+    expected_table = expected_writer.ProcessTableAndEndWrite();
+    pkb::PKBRead expected_reader(std::move(expected_table));
+    auto expected_ftr = std::make_unique<filter::ParentPredicateFilter>(
+        [](pkb::ParentData data) { return true; });
+    auto expected_results_ptr = expected_reader.Parent(std::move(expected_ftr));
+    auto expected_results = *(expected_results_ptr->get_result());
+
+    REQUIRE(actual_results == expected_results);
+  }
+
+  SECTION("Two if statements - relationship only holds for same branch and statement") {
+    std::string program = "procedure parent { if (x == 0) then { read x; } else { read y; } if (y == x) then { read y; } else { read x; } }";
+
+    auto actual_results = InitializeActualResult(program);
+
+    std::unique_ptr<pkb::PKBRelationTable> expected_table =
+        std::make_unique<pkb::PKBRelationTable>();
+    pkb::PKBWrite expected_writer(std::move(expected_table));
+    expected_writer.AddParentData(1, 2);
+    expected_writer.AddParentData(1, 3);
+    expected_writer.AddParentData(4, 5);
+    expected_writer.AddParentData(4, 6);
     expected_table = expected_writer.ProcessTableAndEndWrite();
     pkb::PKBRead expected_reader(std::move(expected_table));
     auto expected_ftr = std::make_unique<filter::ParentPredicateFilter>(
--- Team05/Code05/src/integration_testing/src/SP_PKB/UsesSTest.cpp
+++ Team05/Code05/src/integration_testing/src/SP_PKB/UsesSTest.cpp
@@ -7,15 +7,5 @@
 #include "SP/visitors/UsesVisitor.h"
 
 TEST_CASE("Test SP and PKB integration for Uses data") {
-  SECTION("") {
-    std::string program = "";
-    std::unique_ptr<pkb::PKBRelationTable> expected_results =
-        std::make_unique<pkb::PKBRelationTable>();
-    std::unique_ptr<pkb::PKBRelationTable> actual_results =
-        std::make_unique<pkb::PKBRelationTable>();
-    auto root = sp::SourceProcessor::ParseProgram(program);
-    sp::SourceProcessor::ExtractRelationships(root, actual_results);
 
-    REQUIRE(actual_results == expected_results);
-  }
 }

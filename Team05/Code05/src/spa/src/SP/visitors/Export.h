7829bdf75a66ed1ace7be7dac55a63f50bd65dcb
okg00dbye <E0425498@u.nus.edu>
2023-03-11 20:58:59 +0800

    Move visitors into an Export file

---
+++ Team05/Code05/src/spa/src/SP/visitors/Export.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "SP/visitors/AssignVisitor.h"
+#include "SP/visitors/CallsVisitor.h"
+#include "SP/visitors/DataVisitor.h"
+#include "SP/visitors/FollowsVisitor.h"
+#include "SP/visitors/ModifiesVisitor.h"
+#include "SP/visitors/ParentVisitor.h"
+#include "SP/visitors/UsesVisitor.h"

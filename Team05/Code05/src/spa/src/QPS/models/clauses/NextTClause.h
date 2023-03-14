6bfc4d63ca8e10d1b85406b3ab12efef3fe4eaa4
Au Chen Xi, Gabriel Au <e0544006@u.nus.edu>
2023-03-14 11:16:27 +0800

    Implement initial nexttclause

---
+++ Team05/Code05/src/spa/src/QPS/models/clauses/NextTClause.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <memory>
+#include <queue>
+#include <utility>
+
+#include "Clause.h"
+
+namespace qps {
+
+class NextTClause : public Clause {
+ public:
+  NextTClause(ArgumentPtr arg1, ArgumentPtr arg2)
+      : Clause(std::move(arg1), std::move(arg2)) {}
+
+  inline void Index(const Entity& index, const pkb::PKBReadPtr& pkb,
+                    EntitySet& results) override {
+    Clause::Index<pkb::NextData>(
+        index,
+        [&](Entity::Value key) {
+          auto filter = std::make_unique<NextIndexFilter>(key);
+          return std::move(pkb->Next(std::move(filter))->get_result());
+        },
+        [&](EntitySet& result, pkb::NextData data) {
+          std::unordered_set<int> visited;
+          std::queue<int> frontier;
+          for (auto& l : data.get_next_im_list()) {
+            frontier.push(l);
+            result.insert(Entity(l));
+          }
+          while (!frontier.empty()) {
+            int c = frontier.top();
+            frontier.pop();
+            auto d = pkb->Next(NextIndexFilter::of(c));
+            for (auto& l : d.get_next_im_list()) {
+              frontier.push(l);
+              result.insert(Entity(l));
+            }
+          }
+        },
+        results);
+  }
+};
+
+}  // namespace qps

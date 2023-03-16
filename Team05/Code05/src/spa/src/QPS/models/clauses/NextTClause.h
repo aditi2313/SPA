3ab53b8256624177355d31d9dc04de697a653d16
Au Chen Xi, Gabriel <au.gabriel@yahoo.com.sg>
2023-03-16 17:55:57 +0800

    Switch logic to nextt

--- Team05/Code05/src/spa/src/QPS/models/clauses/NextTClause.h
+++ Team05/Code05/src/spa/src/QPS/models/clauses/NextTClause.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <memory>
 #include <queue>
 #include <unordered_set>
@@ -18,34 +19,11 @@ class NextTClause : public Clause {
 
   inline void Index(const Entity& index, const pkb::PKBReadPtr& pkb,
                     EntitySet& results) override {
-    Clause::Index<pkb::NextData>(
-        index,
-        [&](Entity::Value key) {
-          auto filter = std::make_unique<NextIndexFilter>(key);
-          return std::move(pkb->Next(std::move(filter))->get_result());
-        },
-        [&](EntitySet& result, pkb::NextData data) {
-          std::unordered_set<int> visited;
-          std::queue<int> frontier;
-          for (auto& l : data.get_next_im_list()) {
-            frontier.push(l);
-            result.insert(Entity(l));
-          }
-          while (!frontier.empty()) {
-            int c = frontier.front();
-            frontier.pop();
-            if (visited.count(c)) continue;
-            visited.insert(c);
-            auto d = pkb->Next(NextIndexFilter::of(c));
-            auto res = d->get_result();
-            if (!res->exists(c)) continue;
-            for (auto& l : res->get_row(c).get_next_im_list()) {
-              frontier.push(l);
-              result.insert(Entity(l));
-            }
-          }
-        },
-        results);
+    assert(index.is_int());
+    auto res = pkb->NextT(index.get_int());
+    for (auto v : res) {
+      results.insert(Entity(v));
+    }
   }
 };
 

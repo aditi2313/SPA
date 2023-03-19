c7323f67a41d40fa026e28db2c13e388810ec36a
Au Chen Xi, Gabriel <au.gabriel@yahoo.com.sg>
2023-03-19 11:19:20 +0800

    Reverse formatting changes

--- Team05/Code05/src/spa/src/PKB/data/UsesData.h
+++ Team05/Code05/src/spa/src/PKB/data/UsesData.h
@@ -1,8 +1,9 @@
 #pragma once
-#include <iostream>
 #include <string>
-#include <unordered_set>
+#include <iostream>
 #include <variant>
+#include <unordered_set>
+
 
 namespace pkb {
 
@@ -11,30 +12,32 @@ class UsesData {
   UsesData(std::variant<int, std::string> line,
            const std::unordered_set<std::string>& variable_names);
 
-  friend bool operator<(const UsesData& LHS, const UsesData& RHS) {
-    return LHS.line_ < RHS.line_ ||
-           (LHS.line_ == RHS.line_ &&
-            std::lexicographical_compare(
-                LHS.variable_names_.begin(), LHS.variable_names_.end(),
-                RHS.variable_names_.begin(), RHS.variable_names_.end()));
-  }
 
-  friend bool operator==(const UsesData& LHS, const UsesData& RHS) {
-    return LHS.line_ == RHS.line_ && LHS.variable_names_ == RHS.variable_names_;
-  }
+    friend bool operator<(const UsesData& LHS, const UsesData& RHS) {
+        return LHS.line_ < RHS.line_ ||
+               (LHS.line_ == RHS.line_ &&
+                std::lexicographical_compare(LHS.variable_names_.begin(),
+                                             LHS.variable_names_.end(),
+                                             RHS.variable_names_.begin(),
+                                             RHS.variable_names_.end()));
+    }
 
-  inline std::variant<int, std::string> get_index() { return line_; }
+    friend bool operator==(const UsesData& LHS, const UsesData& RHS) {
+        return LHS.line_ == RHS.line_ &&
+        LHS.variable_names_ == RHS.variable_names_;
+    }
 
-  inline void add_variables(std::unordered_set<std::string> vars) {
-    variable_names_.merge(vars);
-  }
+    inline std::variant<int, std::string> get_index() { return line_; }
 
-  inline const std::unordered_set<std::string>& get_variables() {
-    return variable_names_;
-  }
-
+    inline const std::unordered_set<std::string>& get_variables() {
+        return variable_names_;
+    }
+    inline void add_variables(std::unordered_set<std::string> vars) {
+        variable_names_.merge(vars);
+    }
  private:
-  std::variant<int, std::string> line_;
-  std::unordered_set<std::string> variable_names_;
+    std::variant<int, std::string> line_;
+    std::unordered_set<std::string> variable_names_;
 };
 }  // namespace pkb
+

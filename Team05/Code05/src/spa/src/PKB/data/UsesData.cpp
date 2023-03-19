891af4dbe09bd250341b386a5c5f0c368d512fbe
aditi2313 <guptaaditi2313@gmail.com>
2023-03-19 23:51:53 +0800

    Subclass changes.

--- Team05/Code05/src/spa/src/PKB/data/UsesData.cpp
+++ Team05/Code05/src/spa/src/PKB/data/UsesData.cpp
@@ -4,5 +4,6 @@
 namespace pkb {
 pkb::UsesData::UsesData(std::variant<int, std::string> line,
                         const std::unordered_set<std::string>& variable_names)
-    : Data<std::variant<int, std::string>>(line), variable_names_(variable_names) {}
+    : Data<std::variant<int, std::string>>(line),
+    variable_names_(variable_names) {}
 }  // namespace pkb

9fee3521a848d1b52b6a38deae799264b8744f33
aditi2313 <guptaaditi2313@gmail.com>
2023-03-29 15:56:27 +0800

    DataWrapper and Data changes.

--- Team05/Code05/src/spa/src/PKB/data/Data.h
+++ Team05/Code05/src/spa/src/PKB/data/Data.h
@@ -4,13 +4,22 @@
 #include "DataWrapper.h"
 
 namespace pkb {
-    template <class T>
-    class Data : public DataWrapper<T> {
-    public:
-        explicit Data(T line);
-    };
+template <class T>
+class Data : public DataWrapper {
+ public:
+    explicit Data(T line);
 
-    template<class T>
-    Data<T>::Data(T line)
-            : DataWrapper<T>(line) {}
+    inline T get_index() { return line_; }
+
+    inline std::variant<int, std::string> get_key() {
+        return line_;
+    }
+
+protected:
+    T line_;
+};
+
+template<class T>
+Data<T>::Data(T line)
+    : line_(line) {}
 }  // namespace pkb
--- Team05/Code05/src/spa/src/PKB/data/DataWrapper.h
+++ Team05/Code05/src/spa/src/PKB/data/DataWrapper.h
@@ -2,21 +2,12 @@
 
 #include <cassert>
 #include <variant>
+#include <string>
 #include "DataWrapper.h"
 
 namespace pkb {
-    template <class Key = std::variant<int, std::string>>
-    class DataWrapper {
-    public:
-        explicit DataWrapper(Key line);
-
-        inline Key get_index() { return line_; }
-
-    protected:
-        Key line_;
-    };
-
-    template<class Key>
-    DataWrapper<Key>::DataWrapper(Key line)
-            : line_(line) {}
+class DataWrapper {
+ public:
+    virtual std::variant<int, std::string> get_key() = 0;
+};
 }  // namespace pkb

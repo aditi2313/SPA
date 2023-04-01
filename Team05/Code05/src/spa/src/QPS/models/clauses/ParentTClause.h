725040b05b784b5eeb19280aac38dc3fbeb0d4f4
Au Chen Xi, Gabriel <au.gabriel@yahoo.com.sg>
2023-04-01 14:21:14 +0800

    Add optimisation for followsT and ParentsT [with stats]

--- Team05/Code05/src/spa/src/QPS/models/clauses/ParentTClause.h
+++ Team05/Code05/src/spa/src/QPS/models/clauses/ParentTClause.h
@@ -10,10 +10,10 @@ using filter::ParentIndexFilter;
 
 namespace qps {
 // RS between statements (transitive)
-class ParentTClause : public Clause {
+class ParentTClause : public TClause {
  public:
   ParentTClause(ArgumentPtr arg1, ArgumentPtr arg2)
-      : Clause(
+      : TClause(
       ClauseType::kParentT, std::move(arg1), std::move(arg2)) {}
 
   inline void Index(const Entity &index,
@@ -25,6 +25,17 @@ class ParentTClause : public Clause {
     auto& data = result.read_data();
     AddList(data.get_all_children(), results);
   }
+
+  bool TIndex(const Entity &index, const pkb::PKBReadPtr &pkb,
+                      const Entity &reference) override {
+    auto filter = filter::ParentIndexFilter(index.get_int());
+    auto &reader = pkb->Parent(filter);
+    if (reader.reached_end()) return false;
+    auto &data = reader.read_data();
+    auto &calls = data.get_all_children();
+    return calls.count(reference.get_int());
+  }
+
 };
 
 }  // namespace qps
